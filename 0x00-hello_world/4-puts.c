e1b97d5b32c91953472ad3626d29d3d22451d6e9
nomakhosi17 <ndlovunomakhosi250@gmail.com>
2023-09-07 08:21:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
-#include <stdio.h>
-
+ #include <stdio.h>
 
 /**
- *
- *  * main - this is the main put function
- *
- *   * Return: 0
- *
- *    */
-
+ * main - this is the main put function
+ * Return: 0
+ */
 int main(void)
-
 {
-
 	        puts("\"Programming is like building a multilingual puzzle");
-
 		                return (0);
-
 }
