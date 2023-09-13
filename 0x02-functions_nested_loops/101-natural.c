a51da9cc9c9b935e7014421eca8d30b26c6fd0fa
nomakhosi17 <ndlovunomakhosi250@gmail.com>
2023-09-13 08:01:40 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code
  * Return: Always 0.
  */
+
 int main(void)
 {
 	print_times_table(3);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,3 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#endif
